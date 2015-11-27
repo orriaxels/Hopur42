1c0f012eec3b2a4628baf8a1dde9c95f32eb77e1
orriaxels <orriaxels@gmail.com>
2015-11-27 11:54:26 +0000

    breyting á menu

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ void printMenu();
 
 int main()
 {
-
-
   //  do
     printMenu();
 //    function print menu
@@ -22,14 +20,14 @@ int main()
 void printMenu()
 {
 
-    cout << "\tWelcome to this awesome database" << endl;
+    cout << " Welcome to this awesome database" << endl;
     cout << endl;
-    cout << "What would you like to do?" << endl;
-    cout << endl;
-    cout << "1. Add a new entry" << endl;
-    cout << "2. Display the database" << endl;
-    cout << "3. Search the database" << endl;
-    cout << "4. Quit" << endl;
+    cout << " What would you like to do?" << endl;
+    //cout << endl;
+    cout << "  1. Add a new entry" << endl;
+    cout << "  2. Display the database" << endl;
+    cout << "  3. Search the database" << endl;
+    cout << "  4. Quit" << endl;
     cout << endl;
     cout << "Please select an option from the list: ";
 
@@ -37,7 +35,7 @@ void printMenu()
 
 /*
 
-    fucntion getUserSelection with error check
+    function getUserSelection with error check
     switch
         function Search
         Fucntion display
