cf863fcb2b831e13d7a7d71b7a711eb26db51f78
orriaxels <orriaxels@gmail.com>
2015-11-27 11:28:32 +0000

    menu klár
    menu fall komið inn

--- main.cpp
+++ main.cpp
@@ -3,17 +3,39 @@
 
 using namespace std;
 
+void printMenu();
+
 int main()
 {
-    do
+
+
+  //  do
+    printMenu();
 //    function print menu
 //    getuserselection
-    while(!quit)
+//    while();
 
 
     return 0;
 }
-/*     
+
+void printMenu()
+{
+
+    cout << "\tWelcome to this awesome database" << endl;
+    cout << endl;
+    cout << "What would you like to do?" << endl;
+    cout << endl;
+    cout << "1. Add a new entry" << endl;
+    cout << "2. Display the database" << endl;
+    cout << "3. Search the database" << endl;
+    cout << "4. Quit" << endl;
+    cout << endl;
+    cout << "Please select an option from the list: ";
+
+}
+
+/*
 
     fucntion getUserSelection with error check
     switch
