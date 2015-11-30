d45f5e6dbecad341db175dcbbd3d9a748d523c59
gretarm <gretarm15@ru.is>
2015-11-30 15:57:24 +0000

    Merge conflict persons

--- persons.cpp
+++ persons.cpp
@@ -6,59 +6,64 @@ using namespace std;
 
 
 Persons::Persons(){
-//Default constructor
-    firstname="";
-    lastname="";
-    yearBorn=0;//Jesus
-    yearDied=33; // Jesus died at 33
+	firstname="";
+	lastname="";
+	yearBorn=0;//Jesus 
+	yearDied=0; 
 }
 
 Persons::Persons(string fName, string lName, int born, int died){
-//Constructor without quote
-    firstname=fName;
-    lastname=lName;
-    yearBorn=born;
-    yearDied=died;
+	firstname=fName;
+	lastname=lName;
+	yearBorn=born; 
+	yearDied=died;	
 }
 
 Persons::Persons(string fName, string lName, int born, int died, string Quote){
-//Constructor with quote
-    firstname=fName;
-    lastname=lName;
-    yearBorn=born;
-    yearDied=died;
-    quote=Quote;
+	firstname=fName;
+	lastname=lName;
+	yearBorn=born; 
+	yearDied=died;
+	quote=Quote;	
 }
 
-string Persons::getF(string firstname){
-    firstname = firstname;
-
-    retrun firstname;
+//get funcitons
+string Persons::getF(){
+	return firstname;
 }
 
-string Persons::getL(string lastname){
-    lastname = lastname;
-
-    return lastname;
+string Persons::getL(){
+	return lastname;
 }
-string Persons::getQuote(string quote){
-    quote = qoute;
-    return quote;
-}
-bool Persons::getGender(bool gender){
-    gender = gender;
-
+bool Persons::getGender(){
     return gender;
 }
-int Persons::getYearBorn(int yearborn){
-    yearborn = yearborn
-
-    return yearBorn;
+string Persons::getQuote(){
+	return quote;
 }
-int Persons::getYearDied(int yeardied){
-    yeardied = yeardied;
-
-    return yearDied;
+int Persons::getYearBorn(){
+	return yearBorn;
+} 
+int Persons::getYearDied(){
+	return yearDied;
 }
 
-
+//Set funcitons
+void Persons::setFirst(const string firstName){
+	firstname=firstName;
+}
+void Persons::setLast(const string lastName){
+	lastname=lastName;
+}
+void Persons::setQuote(const string persQuote){
+	quote=persQuote;
+}
+void Persons::setGender(const bool persGender){
+	gender=persGender;
+}
+void Persons::setBorn(const int yBorn){
+	yearBorn=yBorn;
+}
+void Persons::setDied(const int yDied){
+	yearDied=yDied;
+}
