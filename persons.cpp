0ceee2c4ae9b7bd9ec6b3adc9e4cdd9fe98f04b1
gretarm <gretarm15@ru.is>
2015-12-01 07:51:49 +0000

    Revert "Persons.cpp"
    This reverts commit 5ccb6eee986bda3ff5b6f50fc3ad5d31016d1a9a.

--- persons.cpp
+++ persons.cpp
@@ -6,67 +6,59 @@ using namespace std;
 
 
 Persons::Persons(){
-	firstname="";
-	lastname="";
-	gender=0;
-	yearBorn=0;//Jesus 
-	yearDied=0; 
+//Default constructor
+    firstname="";
+    lastname="";
+    yearBorn=0;//Jesus
+    yearDied=33; // Jesus died at 33
 }
 
 Persons::Persons(string fName, string lName, int born, int died){
-	firstname=fName;
-	lastname=lName;
-	gender=0;
-	yearBorn=born; 
-	yearDied=died;	
+//Constructor without quote
+    firstname=fName;
+    lastname=lName;
+    yearBorn=born;
+    yearDied=died;
 }
 
 Persons::Persons(string fName, string lName, int born, int died, string Quote){
-	firstname=fName;
-	lastname=lName;
-	gender=0;
-	yearBorn=born; 
-	yearDied=died;
-	quote=Quote;	
+//Constructor with quote
+    firstname=fName;
+    lastname=lName;
+    yearBorn=born;
+    yearDied=died;
+    quote=Quote;
 }
 
+string Persons::getF(string firstname){
+    firstname = firstname;
 
-//get funcitons
-string Persons::getF(){
-	return firstname;
+    retrun firstname;
 }
-string Persons::getL(){
-	return lastname;
+
+string Persons::getL(string lastname){
+    lastname = lastname;
+
+    return lastname;
 }
-bool Persons::getGender(){
+string Persons::getQuote(string quote){
+    quote = qoute;
+    return quote;
+}
+bool Persons::getGender(bool gender){
+    gender = gender;
+
     return gender;
 }
-string Persons::getQuote(){
-	return quote;
+int Persons::getYearBorn(int yearborn){
+    yearborn = yearborn
+
+    return yearBorn;
 }
-int Persons::getYearBorn(){
-	return yearBorn;
-} 
-int Persons::getYearDied(){
-	return yearDied;
+int Persons::getYearDied(int yeardied){
+    yeardied = yeardied;
+
+    return yearDied;
 }
 
-//Set funcitons
-void Persons::setFirst(const string firstName){
-	firstname=firstName;
-}
-void Persons::setLast(const string lastName){
-	lastname=lastName;
-}
-void Persons::setQuote(const string persQuote){
-	quote=persQuote;
-}
-void Persons::setGender(const bool persGender){
-	gender=persGender;
-}
-void Persons::setBorn(const int yBorn){
-	yearBorn=yBorn;
-}
-void Persons::setDied(const int yDied){
-	yearDied=yDied;
-}
+
